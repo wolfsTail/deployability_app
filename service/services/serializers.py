59ccb2c71c5af91d3d803c9b7518eeca86e9c109
DRomanov <lukaviy2la@gmail.com>
2024-03-14 20:14:30 +0300

    использование prefetch_related для решения проблемы n+1

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = '__all__'
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.company_name', read_only=True)
     email = serializers.CharField(source='client.user.email', read_only=True)
+    plan = PlanSerializer(read_only=True)
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return (instance.service.full_price - 
+                instance.service.full_price * (instance.plan.discount_percent / 100))
+
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
+
